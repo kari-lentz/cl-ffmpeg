32ce8ceaf3af55604b19cc04db73be8e97d2a66b
Kari Lentz <karilentz@att.net>
2013-07-08 21:59:44 -0400

    with-foreign-ring-buffer duplicate export eliminated

--- src/packages.lisp
+++ src/packages.lisp
@@ -1,7 +1,7 @@
 (defpackage :ring-buffer
   (:documentation "front end for cl-ffmpeg")
   (:use :cl :cffi :bordeaux-threads :my-env :my-db :utility)
-  (:export :with-foreign-ring-buffer :with-foreign-ring-buffer :run))
+  (:export :with-foreign-ring-buffer :run))
 
 (defpackage :cl-ffmpeg
   (:documentation "front end for cl-ffmpeg")
